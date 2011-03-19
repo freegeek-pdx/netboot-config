b5df25d9c0de0f90555f87cbeb9923fa5bc731ff
ryan52 <ryan52@c57b91e5-1707-0410-b08f-e6c7fdc2b75e>
2011-03-19 19:06:27 +0000

    zhora, not arik
    git-svn-id: svn+ssh://dev.freegeek.org/svn/ass/trunk/configs/build@3009 c57b91e5-1707-0410-b08f-e6c7fdc2b75e

--- iptables.sh
+++ iptables.sh
@@ -5,5 +5,5 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 /sbin/iptables -F
 /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 /sbin/iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -A FORWARD -d 192.168.253.93 -i eth1 -o eth0 -j ACCEPT
+/sbin/iptables -A FORWARD -d 192.168.253.94 -i eth1 -o eth0 -j ACCEPT
 /sbin/iptables -A FORWARD -j REJECT
